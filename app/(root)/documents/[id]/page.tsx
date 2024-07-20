0835a66247d4cb5effe5e908bf1ad087d585c5d3
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-07-20 23:14:30 +0530

    Update document page layout and add header component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 
 const Document = () => {
-  return (
-    <div>
-      Document Page
-    </div>
-  )
-}
+    return (
+        <div>
+            <Header>
+                <div className="flex w-fit items-center justify-center gap-2">
+                    <p className="document-title">This is a document title</p>
+                </div>
+            </Header>
+
+            <Editor />
+        </div>
+    );
+};
 
 export default Document;
