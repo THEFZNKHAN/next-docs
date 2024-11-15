e27521bc409fe8fc6b0a9e1e96831ea562040635
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-11-16 02:00:44 +0530

    Add user mention suggestions for collaborative editing

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -6,15 +6,27 @@ import {
     LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/users.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/users.actions";
+import { EmailAddress } from "@clerk/nextjs/server";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+    const { user: clerkUser } = useUser();
+
     return (
         <LiveblocksProvider
             authEndpoint="/api/liveblocks-auth"
             resolveUsers={async ({ userIds }) => {
                 const users = await getClerkUsers({ userIds });
                 return users;
+            }}
+            resolveMentionSuggestions={async ({ text, roomId }) => {
+                const roomUsers = await getDocumentUsers({
+                    roomId,
+                    currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+                    text,
+                });
+                return roomUsers;
             }}
         >
             <ClientSideSuspense fallback={<Loader />}>
